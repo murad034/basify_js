c9a52e4aad38724b49ebb22f529388d97fb4d7b5
Karima Jaman Moon <karimajaman9876@gmail.com>
2024-12-04 00:06:06 +0600

    class 10 starting

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-let button = document.querySelector("#btn1");
-let para1 = document.querySelector("#para1");
+const p = document.querySelector("#para1");
 
-button.addEventListener('click', function(){
-    //alert("button is working");
-    para1.textContent = "Button is clicked";
-    //para1.style.color = "red";
-    //para1.style.backgroundColor="green";
-    
-    //para1.classList.add("p-color");
-    para1.setAttribute("class", "p-color");
-    para1.setAttribute("href", "google.com");
-    let href = para1.getAttribute("href");
-    let ip = para1.getAttribute("ip");
-    console.log(href, ip);
-});
+function clickBtn(){
+    p.append(" hello append");
+    p.prepend("hi prepend ");
+    p.before("hee before ");
+    p.after("hlw after ");
+}
