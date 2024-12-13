5df8e45865d4f945ebba101f3e148ceef6f4c6a3
Karima Jaman Moon <karimajaman9876@gmail.com>
2024-12-13 19:34:20 +0600

    class 11 basify finished

--- script.js
+++ script.js
@@ -18,4 +18,56 @@ iconimg.addEventListener("mouseover", function(){
 
 iconimg.addEventListener("mouseout", function(){
     mp.textContent = "";
-})
+});
+
+function dbClick(){
+    alert("ok");
+}
+
+const username = document.querySelector("#username");
+const errorPara = document.querySelector("#errorPara");
+errorPara.style.color = "red";
+// username.addEventListener("input", function(){
+username.addEventListener("keyup", function(){
+    if(this.value.length <= 3){
+        errorPara.textContent = "Input must be 3 length characters";
+        return false;
+    }
+    errorPara.textContent = "";
+    return true;
+});
+
+
+// form submit
+
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const formError = document.querySelector("#formError");
+
+function validate(){
+    if(fullname.value == "" || email.value == "" || password.value == ""){
+        fullname.placeholder = "Fullname is required";
+        email.placeholder = "Email is required";
+        password.placeholder = "Password is required";
+    }
+
+    if(fullname.value == ""){
+        //formError.textContent = "Fullname is required";
+        fullname.placeholder = "Fullname is required";
+        return false;
+    }
+
+    if(email.value == ""){
+        formError.innerText = "Email is required";
+        return false;
+    }
+
+    if(password.value == ""){
+        formError.innerHTML = "Password is required";
+        return false;
+    }
+
+    alert("your form has been submitted. Thank You");
+        
+}
